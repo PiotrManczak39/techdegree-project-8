ee2f9eb07b75dbaaaef84980cbdb78b08b40402f
Piotr Manczak <51323846+PiotrManczak39@users.noreply.github.com>
2019-11-06 01:18:29 +0000

    Ready for Meets Expectarions submission

--- js/appbis.js
+++ js/appbis.js
@@ -1,6 +1,7 @@
 const mainContent = document.getElementById('main-content');
 const employeeArrays = [];
 const profiles = document.getElementsByClassName('profile');
+const modal = document.getElementById('modal');
 let index = 0;
 
 fetch('https://randomuser.me/api/?results=12')
@@ -17,10 +18,12 @@ function generateEmployees(data) {
   data.map(result => {
     const html = `
       <section class='card' index = ${index}>
-        <img class='image' src='${result.picture.large}' alt = 'profile-image'>
-        <h3 class='employee-info'>${result.name.first} ${result.name.last}</h3>
-        <p class='employee-info'>${result.email}</p>
-        <p class='employee-info'>${result.location.city}</p>
+        <img class='image' src='${result.picture.large}' alt='profile-image'>
+        <div class="card-info">
+          <h3 class='employee-info'>${result.name.first} ${result.name.last}</h3>
+          <p class='employee-info'>${result.email}</p>
+          <p class='employee-info'>${result.location.city}</p>
+        </div>
       </section>
     `
     index += 1;
@@ -33,26 +36,47 @@ function generateModal(number) {
   let employee = employeeArrays[number];
   let html = `
     <div class="modal-inside">
-      <img src="${employee.picture.large}" />
-      <h3>${employee.name.first} ${employee.name.last}</h3>
-      <p>${employee.email}</p>
-      <p>${employee.location.city}</p>
-      <p>${employee.phone}</p>
-      <p>${employee.location.street.number}  ${employee.location.street.name}, ${employee.location.postcode}</p>
-      <p>Birthday: ${employee.dob.date}</p>
+      <div class="wrap">
+        <img src="${employee.picture.large}" />
+        <h3>${employee.name.first} ${employee.name.last}</h3>
+        <p>${employee.email}</p>
+        <p class="location">${employee.location.city}</p>
+        <p>${employee.phone}</p>
+        <p>${employee.location.street.number}  ${employee.location.street.name}, ${employee.location.postcode}</p>
+        <p>Birthday: ${employee.dob.date}</p>
+      </div>
     </div>
   `
   modal.innerHTML = html;
+  let modalInside = document.querySelector('.modal-inside');
+  const closeModal = document.createElement('span');
+  closeModal.className = 'closeModal';
+  closeModal.textContent = '+';
+  modalInside.appendChild(closeModal);
 }
 
+modal.style.display = 'none';
+
 mainContent.addEventListener('click', (e) => {
-  console.log(event.target);
   if (event.target.className === 'card') {
     let cel = event.target.getAttribute('index');
     generateModal(cel);
-  } else if (event.target.className === 'image' || event.target.className === 'employee-info') {
+    modal.style.display = 'flex';
+  } else if ( event.target.className === 'employee-info') {
+    let parent = event.target.parentNode.parentNode;
+    let cel = parent.getAttribute('index');
+    generateModal(cel);
+    modal.style.display = 'flex';
+  } else if (event.target.className === 'image' || event.target.className === 'card-info') {
     let parent = event.target.parentNode;
     let cel = parent.getAttribute('index');
     generateModal(cel);
+    modal.style.display = 'flex';
+  }
+});
+
+modal.addEventListener('click', (e) => {
+  if (event.target.className === 'closeModal') {
+    modal.style.display = 'none';
   }
 });
